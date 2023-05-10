8e4a67282fe4b0e18bfb9957eac739ac8c9bde73
끼얏호우끼얏호우 <jiwooson.0816@gmail.com>
2023-05-10 12:08:17 +0900

    들켜뿟노

---
+++ Ang Kimochi.swift
@@ -0,0 +1,22 @@
+//
+//  Ang Kimochi.swift
+//  first project
+//
+//  Created by sonjiwoo on 2023/05/10.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Image("들켜뿟노")
+        
+        
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
