3ace0ba4bf308fa0ffe0f7dede7b1c7761a79616
yedon seo <seojangwon341@gmail.com>
2023-05-10 10:24:49 +0900

    Initial Commit

---
+++ first project/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  first project
+//
+//  Created by 서예돈 on 2023/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var gauge: Double = 0.0
+    @State var isBye: Bool = true
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Bye, world!")
+            Text("Bye, world!")
+            Slider(value: $gauge)
+            Toggle(isOn: $isBye) {
+                Text("Bye, world!")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first project/first_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_projectApp.swift
+//  first project
+//
+//  Created by 서예돈 on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct first_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
